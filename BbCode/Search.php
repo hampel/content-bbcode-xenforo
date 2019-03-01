2ba41a699e03b9f5d1a4f46209a072356f9b784a
Simon Hampel <simon@hampelgroup.com>
2019-03-01 13:37:34 +1100

    SEARCH BBCode

---
+++ BbCode/Search.php
@@ -0,0 +1,148 @@
+<?php namespace Hampel\ContentBbCode\BbCode;
+
+use XF\Repository\Tag;
+
+class Search
+{
+	public static function renderTagSearch($tagChildren, $tagOption, $tag, array $options, \XF\BbCode\Renderer\AbstractRenderer $renderer)
+	{
+		$options['noFollowUrl'] = true;
+
+		if (empty($tagOption))
+		{
+			$term = $renderer->renderSubTreePlain($tagChildren);
+			$text = $renderer->filterString($term, ['stopSmilies' => 1, 'stopBreakConversion' => 1] + $options);
+			$text = htmlspecialchars($text);
+			$type = 'forum';
+		}
+		else
+		{
+			$parts = explode(';', $tagOption);
+			foreach ($parts AS &$part)
+			{
+				$part = trim($part);
+				$part = str_replace(' ', '', $part);
+			}
+
+			if (count($parts) == 1)
+			{
+				$term = $renderer->renderSubTreePlain($tagChildren);
+
+				$text = $renderer->filterString($term, ['stopSmilies' => 1, 'stopBreakConversion' => 1] + $options);
+				$text = htmlspecialchars($text);
+
+				$type = $renderer->filterString(array_shift($parts), ['stopSmilies' => 1, 'stopBreakConversion' => 1] + $options);
+			}
+			else
+			{
+				$text = $renderer->renderSubTree($tagChildren, $options);
+				$type = $renderer->filterString(array_shift($parts), ['stopSmilies' => 1, 'stopBreakConversion' => 1] + $options);
+
+				$term = $renderer->filterString(array_shift($parts), ['stopSmilies' => 1, 'stopBreakConversion' => 1] + $options);
+			}
+		}
+
+		$term = urlencode($term);
+		$router = \XF::app()->router('public');
+		$formatter = \XF::app()->stringFormatter();
+
+		// TODO: add more descriptive text to tags without body text, eg "Search Results for Query: <keyword>"
+		// TODO: custom search options
+
+		switch ($type)
+		{
+			case 'forum':
+			case 'forums':
+				$url = $router->buildLink('canonical:search/search', [], ['keywords' => $term]);
+				break;
+			case 'thread':
+			case 'threads':
+				$url = $router->buildLink('canonical:search/search', [], ['keywords' => $term, 'type' => 'thread']);
+				break;
+			case 'post':
+			case 'posts':
+				$url = $router->buildLink('canonical:search/search', [], ['keywords' => $term, 'type' => 'post']);
+				break;
+			case 'resource':
+			case 'resources':
+				$url = $router->buildLink('canonical:search/search', [], ['keywords' => $term, 'type' => 'resource_update']);
+				break;
+			case 'media':
+				$url = $router->buildLink('canonical:search/search', [], ['keywords' => $term, 'type' => 'xengallery_media']);
+				break;
+			case 'tag':
+			case 'tags':
+
+				/** @var Tag $tagRepo */
+				$tagRepo = \XF::repository('XF:Tag');
+				$term = $tagRepo->normalizeTag($term);
+				$term = preg_replace('/[^a-zA-Z0-9_ -]/', '', utf8_romanize(utf8_deaccent($term)));
+				$term = preg_replace('/[ -]+/', '-', $term);
+
+				$url = $router->buildLink('canonical:tags', ['tag_url' => $term]);
+				break;
+			case 'site':
+				$boardUrl = parse_url(\XF::options()->boardUrl);
+				$term .= " site:" . preg_replace('#^www\.(.+\.)#i', '$1', $boardUrl['host']);
+				$url = 'https://www.google.com/search?' . http_build_query(['q' => $term]);
+				break;
+			case 'web':
+				$url = 'https://www.google.com/search?' . http_build_query(['q' => $term]);
+				break;
+			case 'image':
+			case 'images':
+				$url = 'https://www.google.com/search?' . http_build_query(['q' => $term, 'tbm' => 'isch']);
+				break;
+			case 'map':
+			case 'maps':
+				$url = 'https://www.google.com/maps?' . http_build_query(['q' => $term]);
+				break;
+			case 'video':
+			case 'videos':
+				$url = 'https://www.google.com/search?' . http_build_query(['q' => $term, 'tbm' => 'vid']);
+				break;
+			case 'news':
+				$url = 'https://www.google.com/search?' . http_build_query(['q' => $term, 'tbm' => 'nws']);
+				break;
+			default:
+				return $text;
+		}
+
+		$linkInfo = $formatter->getLinkClassTarget($url);
+		$rels = [];
+
+		$classAttr = $linkInfo['class'] ? " class=\"$linkInfo[class]\"" : '';
+		$targetAttr = $linkInfo['target'] ? " target=\"$linkInfo[target]\"" : '';
+
+		if (!$linkInfo['trusted'] && !empty($options['noFollowUrl']))
+		{
+			$rels[] = 'nofollow';
+		}
+
+		if ($linkInfo['target'])
+		{
+			$rels[] = 'noopener';
+		}
+
+		$proxyAttr = '';
+		if (empty($options['noProxy']))
+		{
+			$proxyUrl = $formatter->getProxiedUrlIfActive('link', $url);
+			if ($proxyUrl)
+			{
+				$proxyAttr = ' data-proxy-href="' . htmlspecialchars($proxyUrl) . '"';
+			}
+		}
+
+		if ($rels)
+		{
+			$relAttr = ' rel="' . implode(' ', $rels) . '"';
+		}
+		else
+		{
+			$relAttr = '';
+		}
+
+		return '<a href="' . htmlspecialchars($url) . '"' . $targetAttr . $classAttr . $proxyAttr . $relAttr . '>' . $text . '</a>';
+	}
+}
